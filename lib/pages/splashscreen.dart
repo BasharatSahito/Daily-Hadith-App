57ae4fbe410d2637cbd7e7fa19c36d2a98bbf6d0
Basharat <basharataliali3@gmail.com>
2023-10-22 04:23:03 +0500

    Added final Daily Notifications and Hadith UI

---
+++ lib/pages/splashscreen.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToHome();
+  }
+
+  void navigateToHome() {
+    Timer(const Duration(seconds: 1), () {
+      Navigator.pushReplacementNamed(context, "/homepage");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Splash Screen"),
+      ),
+    );
+  }
+}
